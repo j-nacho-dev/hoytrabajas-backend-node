78ceaa189fd630ebce2118f6e677d492b1ed0490
Juan Lopez <juanignaciolopez.dev@gmail.com>
2025-08-07 23:46:08 -0300

    feat: create Express Server

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,21 @@
-console.log('Hola Mundo TS')
+import express from 'express';
+import cors from 'cors';
+
+// Create Express server
+const app = express();
+
+// Enable CORS
+app.use( cors() );
+
+// Public Directory
+app.use( express.static('public') )
+
+// Middleware to parse incoming JSON
+app.use( express.json() )
+
+
+// Listen Port
+const PORT = process.env.PORT || 3000;
+app.listen( PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${ PORT }`)
+} )
